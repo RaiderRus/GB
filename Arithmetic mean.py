299d5d8c16df20245c7df1d60a2b304bd191a88d
raiderrus <raider99@yandex.ru>
2022-03-27 22:03:06 +0300

    delete old files

--- Arithmetic mean.py
+++
@@ -1,5 +0,0 @@
-a, b, c, d = int(input()), int(input()), int(input()), int(input())
-
-am = (a + b + c + d) / 4
-
-print(am)
--- Factorial.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-f = 1
-r = 1
-
-while r <= n:
-    f = f * r
-    r = r + 1
-
-print(f)
