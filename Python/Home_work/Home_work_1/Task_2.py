aadd107156c2214c0a9ed199bdf5a4a76c3aafd1
raiderrus <raider99@yandex.ru>
2022-07-13 00:15:36 +0300

    2 задача решена

--- Python/Home_work/Home_work_1/Task_2.py
+++ Python/Home_work/Home_work_1/Task_2.py
@@ -1,13 +1,6 @@
 # 2- Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-print('Insert coordinates x, y, z:')
-
-x = int(input('x = '))
-y = int(input('y = '))
-z = int(input('z = '))
-
-if not(x or y or z) == (not x) and (not y) and (not z):
-    print(f'The truth of the statement ¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} for all values, the predicate -', True)
-else:
-    print(f'The truth of the statement ¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} for all values, the predicate -', False)
-
+for x in 1, 0:
+    for y in 1, 0:
+        for z in 1, 0:
+            print(f'{x= } {y= } {z= }    result:{not(x or y or z) == (not x and not y and not z)}')
