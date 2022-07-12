ed82f5af9ca70a3fb95fc52ef13d0c3be2e487e2
raiderrus <raider99@yandex.ru>
2022-07-12 22:24:07 +0300

    Task 1 complete

---
+++ Python/Lections/Lection_1.py
@@ -0,0 +1,38 @@
+# print('hello world')
+
+# value = None
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+
+# s = 'Hello \'world\''
+# print(s)    # Вывод строки
+#
+# print(a, '-', b, '-', s)
+# print('{} - {} - {}'.format(a, b, s))
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+#
+# f = False
+# print(f)
+
+# list = [1, 2, 3, 'hello world', True]
+# print(list)
+
+# a = int(input('Введите a: '))
+# b = float(input('Введите b: '))
+# print(a + b)
+
+# a = +123    # унарный плюс
+# b = -321    # унарный минус
+# c = a + b
+# print(c)
+
+a = 1 < 5
+print(a)
+
+func = 1
+T = 4
+x = 123
+print(func < T > x)
